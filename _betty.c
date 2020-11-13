760b927f98027cb1cd490c7ecf961e1fb0400db1
musakanneh <mkanneh17@alustudent.com>
2020-11-14 01:21:08 +0200

    Added soln files with starter functions

---
+++ _betty.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _betty_checks - a function that passes all betty checks
+ * Return: ...
+ */
+
+int __betty_checks()
+{
+    return (0);
+}
---
+++ _simple_shell.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _loop - a function that executes the loop functionalities
+ * Return: Nothing
+ */
+
+void _loop(void)
+{
+
+}
+
+/**
+ * _execute - a function to execute the program
+ * Return: Nothing
+ */
+
+void _execute(void)
+{
+    
+}
+
+/**
+ * main - _simple_shell
+ */
+
+int main(void)
+{
+    pid_t my_pid;
+
+    my_pid = getpid();
+    printf("%d\n", my_pid);
+    return (0);
+}
